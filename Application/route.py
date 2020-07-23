c5d732402553c87c85a94732765c306f8ea00c2b
omris-elda <edmundtheeel@gmail.com>
2020-07-23 14:06:40 +0000

    added some formatting and a navbar

--- Application/route.py
+++ Application/route.py
@@ -2,10 +2,23 @@
 from flask import render_template
 from Application import app
 
+blogData = [
+    {  
+        "name": {"first":"John", "last":"Doe"},
+        "title":"First Post",
+        "content":"This is some blog data for Flask lectures"
+    },
+    {   
+        "name": {"first":"Jane", "last":"Doe"},
+        "title":"Second Post",
+        "content":"This is even more blog data for Flask lectures"
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html", title="Home", posts=blogData)
 
 @app.route("/about")
 def about():
