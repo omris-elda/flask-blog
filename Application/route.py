ec6b910fae47b11274f7308715aa4cc6e5317aec
Omris-Elda <ed.prickett@hotmail.com>
2020-07-27 15:27:28 +0100

    added url_for

--- Application/route.py
+++ Application/route.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
 from Application import app, db, bcrypt
 from Application.models import Posts, Users
 from Application.forms import PostForm, RegistrationForm
