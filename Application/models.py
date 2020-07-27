8d8378d1233dcabeec6fd77e6ab6ec16da975379
Omris-Elda <ed.prickett@hotmail.com>
2020-07-27 14:32:55 +0100

    Corrected a typo

--- Application/models.py
+++ Application/models.py
@@ -15,8 +15,8 @@ class Posts(db.Model):
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.string(100), nullable=False, unique=True)
-    password = db.Column(db.string(500), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(500), nullable=False)
 
     def __repr__(self):
         return "".join(["UserID: ", str(self.id), "\r\n", "Email: ", self.email])
