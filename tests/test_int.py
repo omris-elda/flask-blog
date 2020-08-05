d04375f4a36db4d2df2122b8f46746c8fbacb7d2
Omris-Elda <ed.prickett@hotmail.com>
2020-08-05 15:00:19 +0100

    removed some tests

--- tests/test_int.py
+++ tests/test_int.py
@@ -1,53 +1,53 @@
-import unittest
-import time
-from flask import url_for
-from urllib.request import urlopen
-from os import getenv
-from flask_testing import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from Application import app, db, bcrypt
-from Application.models import Users, Posts
+# import unittest
+# import time
+# from flask import url_for
+# from urllib.request import urlopen
+# from os import getenv
+# from flask_testing import LiveServerTestCase
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from Application import app, db, bcrypt
+# from Application.models import Users, Posts
 
-# setting the test variables for test admin user
-test_admin_first_name =  "admin"
-test_admin_last_name = "admin"
-test_admin_email = "admin@admin.com"
-test_admin_password = "admin"
+# # setting the test variables for test admin user
+# test_admin_first_name =  "admin"
+# test_admin_last_name = "admin"
+# test_admin_email = "admin@admin.com"
+# test_admin_password = "admin"
 
-class TestBase(LiveServerTestCase):
-    # this is the class we'll inherit from before every test so that you start with an empty database
+# class TestBase(LiveServerTestCase):
+#     # this is the class we'll inherit from before every test so that you start with an empty database
 
-    def create_app(self):
-        # config_name = "testing"
-        app.config["SQLALCHEMY_DATABASE_URI"]=getenv("TEST_DB_URI_SECOND")
-        app.config["SECRET_KEY"]=getenv("TEST_SECRET_KEY") or "testing"
-        return app
+#     def create_app(self):
+#         # config_name = "testing"
+#         app.config["SQLALCHEMY_DATABASE_URI"]=getenv("TEST_DB_URI_SECOND")
+#         app.config["SECRET_KEY"]=getenv("TEST_SECRET_KEY") or "testing"
+#         return app
 
-    def setUp(self):
-        # sets up the test driver and creates some users
+#     def setUp(self):
+#         # sets up the test driver and creates some users
 
-        print("<-----NEXT-TEST----->")
-        chrome_options = Options()
-        chrome_options.binary_location = "/l/CA\ Learning/mini_installer.exe"
-        # /usr/bin/chromium-browser
-        chrome_options.add_argument("--headless")
-        # this command just means that it will do everything in the background, instead of
-        # actually opening a browser and showing you what it's doing
-        self.driver = webdriver.Chrome(executable_path="/l/CA\ Learning/flask-blog/chromedriver.exe", chrome_options=chrome_options)
-        # /home/edmundtheeel/chromedriver
-        self.driver.get("http://localhost:5000")
-        db.session.commit()
-        db.drop_all()
-        db.create_all()
+#         print("<-----NEXT-TEST----->")
+#         chrome_options = Options()
+#         chrome_options.binary_location = "/l/CA\ Learning/mini_installer.exe"
+#         # /usr/bin/chromium-browser
+#         chrome_options.add_argument("--headless")
+#         # this command just means that it will do everything in the background, instead of
+#         # actually opening a browser and showing you what it's doing
+#         self.driver = webdriver.Chrome(executable_path="/l/CA\ Learning/flask-blog/chromedriver.exe", chrome_options=chrome_options)
+#         # /home/edmundtheeel/chromedriver
+#         self.driver.get("http://localhost:5000")
+#         db.session.commit()
+#         db.drop_all()
+#         db.create_all()
 
-    def tearDown(self):
-        self.driver.quit()
-        print("<-----END-OF-TEST----->")
+#     def tearDown(self):
+#         self.driver.quit()
+#         print("<-----END-OF-TEST----->")
 
-    def test_server_is_up_and_running(self):
-        response = urlopen("http://127.0.0.0:5000")
-        self.assertEqual(response.code, 200)
+#     def test_server_is_up_and_running(self):
+#         response = urlopen("http://127.0.0.0:5000")
+#         self.assertEqual(response.code, 200)
 
 
 
